257242f3947da11e1983c8fa05fe85f3f17f1d80
mallenm14 <molinaallen62@gmail.com>
2025-03-24 01:55:39 +0800

    added a config for build memory limits

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: "build",
+  experimental: {
+    workerThreads: false,
+    cpus: 4,
+  },
 };
 
 export default nextConfig;
